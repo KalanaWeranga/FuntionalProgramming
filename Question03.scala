667dd497da5f652e795215ee3918a76c68689ba6
KalanaWeranga <91280990+KalanaWeranga@users.noreply.github.com>
2022-08-01 17:17:23 +0530

    Add Question-03

---
+++ Question03.scala
@@ -0,0 +1,26 @@
+import scala.compiletime.ops.string 
+object q3 extends App{
+
+    def toUpper(x:String)={
+        x.toUpperCase();
+    }
+
+    def toLower(y:String)={
+        y.toLowerCase();
+    }
+
+
+    def formatNames(f:String=>String,x:String):String={
+        val a=f(x)
+        return a;
+    }
+
+    print(formatNames(toUpper,"Benny"));
+    println()
+    print(formatNames(toUpper,"Niroshan".slice(0,2))+formatNames(toLower,"Niroshan".slice(2,8)))
+    println();
+    print(formatNames(toLower,"Saman"))
+    println()
+    print(formatNames(toUpper,"Kumara".slice(0,1))+formatNames(toLower,"Kumara".slice(1,5))+formatNames(toUpper,"Kumara".slice(5,6)))
+
+}

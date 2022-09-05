cc33965df056af9af8ed953b2fe8e61d9c19286e
KalanaWeranga <bakweranga@gmail.com>
2022-09-06 02:36:45 +0530

    add case for other characters

--- Tutorial06/Caesar_Cipher.scala
+++ Tutorial06/Caesar_Cipher.scala
@@ -4,11 +4,14 @@ object  Caesar_Cipher extends App{
     val alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
     //encryption function
-    val encryption = (shift:Int,c:Char)=>alphabet((alphabet.indexOf(c.toUpper)+shift)% alphabet.size)
+    val encryption = (shift:Int,c:Char)=>
+        if((alphabet.indexOf(c.toUpper))== -1) c 
+        else alphabet((alphabet.indexOf(c.toUpper)+shift)% alphabet.size)
     
     //decryption function
     val decryption =(shift:Int,c:Char)=> {
-        if((alphabet.indexOf(c.toUpper)-shift)<0) alphabet((alphabet.indexOf(c.toUpper)-shift+alphabet.size)% alphabet.size)
+        if((alphabet.indexOf(c.toUpper))== -1) c
+        else if((alphabet.indexOf(c.toUpper)-shift)<0) alphabet((alphabet.indexOf(c.toUpper)-shift+alphabet.size)% alphabet.size)
         else alphabet((alphabet.indexOf(c.toUpper)-shift)% alphabet.size)
     }
     
@@ -21,16 +24,19 @@ object  Caesar_Cipher extends App{
     println("Enter 1-to encrypt message");
     println("Enter 2-to decrypt message");
     val a=scala.io.StdIn.readInt()
+
+    println("Enter Shift Number:")
+    var shiftnum=scala.io.StdIn.readInt()
     if(a==1){
         // val string = str.replace(" ", "")
         // println("String: "+string)
-        val em = Cipher(encryption, str, 3)
+        val em = Cipher(encryption, str, shiftnum)
         println("Encrypt message :"+em)
     }
     else{
         // val string = str.replace(" ", "")
         // println("String: "+string)
-        val dm = Cipher(decryption, str,3)
+        val dm = Cipher(decryption, str,shiftnum)
         println("Decrypt message :"+dm)
 
     }
